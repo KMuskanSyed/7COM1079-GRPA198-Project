5a0b2ba481dcbed468fa6a4f4dffd59f29b8a3c2
KMuskanSyed <ks25acr@herts.ac.uk>
2025-12-07 16:16:11 +0000

    Created Contigency Table

--- Cleaning_and_Analysis_Script.R
+++ Cleaning_and_Analysis_Script.R
@@ -3,5 +3,15 @@ library(readr)
 Match <- read_csv("Match.csv") #This will observe the dataset i.e columns, rows and column names.
 View(Match)
 Match <- na.omit(Match) #This command handles missing match records and removes the incomplete outcomes.
-#Creating a seperate column in the datset called as "Toss_Won_Match"(i.e Binary Outcome Variable). This operation produces output 1 if Toss_Winner ID equals to Match_Winner ID otherwise 0.This column gives us the clear information about Winner vs Lost
+#Creating a seperate column in the dataset called as "Toss_Won_Match"(i.e Binary Outcome Variable). This operation produces output 1 if Toss_Winner ID equals to Match_Winner ID otherwise 0.This column gives us the clear information about Winner vs Lost
 Match$Toss_Won_Match <- ifelse(Match$Toss_Winner == Match$Match_Winner, 1, 0)
+#contigency table is created to get the draw counts of Toss Winner Wins i.e 1 or Losses i.e 0
+contigency_table <- table(Match$Toss_Won_Match)
+print(contigency_table)
+install.packages("ggplot2")
+#installing package called ggplot2 to feature R graph
+library(ggplot2)
+#Creating data frame for plotting from contigency table
+plot_data <- as.data.frame(contigency_table)
+#Creating contigency table data frame to view as viewable file
+contigency_DF <- as.data.frame(contigency_table)
